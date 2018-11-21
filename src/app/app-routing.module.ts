d8eaddea12715cb5a24b88fc22e6a07b3f460921
godzyken <isgodzy@msn.com>
2018-11-21 21:01:31 +0100

    premier comit
    Signed-off-by: godzyken <isgodzy@msn.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProduitsComponent} from './produits/produits.component';
+import {ProduitDetailComponent} from './produit-detail/produit-detail.component';
+import {ProduitEditComponent} from './produit-edit/produit-edit.component';
+import {ProduitAjoutComponent} from './produit-ajout/produit-ajout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produits',
+    component: ProduitsComponent,
+    data: { title: 'Liste de Produits' }
+  },
+  {
+    path: 'produit-details/:id',
+    component: ProduitDetailComponent,
+    data: { title: 'Détails du Produit' }
+  },
+  {
+    path: 'product-add',
+    component: ProduitAjoutComponent,
+    data: { title: 'Add Produit' }
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProduitEditComponent,
+    data: { title: 'Edit Produit' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProduitsComponent} from './produits/produits.component';
+import {ProduitDetailComponent} from './produit-detail/produit-detail.component';
+import {ProduitAjoutComponent} from './produit-ajout/produit-ajout.component';
+import {ProduitEditComponent} from './produit-edit/produit-edit.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitsComponent,
+    ProduitDetailComponent,
+    ProduitAjoutComponent,
+    ProduitEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
